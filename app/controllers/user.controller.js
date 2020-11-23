9913137bdc2cb049930d9c4d52101d88b544ad38
Bloodsfire <voobsheeee@mail.ru>
2020-11-23 16:57:58 +0600

    Refactor user controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,119 +1,76 @@
-const { Op } = "sequelize";
+import errorHandler from "../utils/errorHandler.js";
 import { models } from '../models/index.js';
 const { User } = models;
 
-const create = (req, res) => {
-    User.create({...req.body})
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the User."
-            });
-        });
-};
-
-const findOne = (req, res) => {
-    const id = req.params.id;
-
-    User.findByPk(id)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving User with id=" + id
-            });
-        });
-};
-
-const findAll = (req, res) => {
-    const title = req.query.title;
-    let condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-
-    User.findAll({ where: condition })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving tutorials."
-            });
-        });
-};
-
-const update = (req, res) => {
-    const id = req.params.id;
-
-    User.update(req.body, {
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "User was updated successfully."
-                });
-            } else {
-                res.send({
-                    message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating User with id=" + id
-            });
-        });
-};
-
-const remove = (req, res) => {
-    const id = req.params.id;
-
-    User.destroy({
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "User was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete User with id=${id}. Maybe User was not found!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete User with id=" + id
-            });
-        });
-};
-
-const removeAll = (req, res) => {
-    User.destroy({
-        where: {},
-        truncate: false
-    })
-        .then(nums => {
-            res.send({ message: `${nums} Users were deleted successfully!` });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while removing all tutorials."
-            });
-        });
-};
 
 export {
     create,
     findOne,
     findAll,
     update,
-    remove,
-    removeAll
+    remove
+}
+
+
+async function create (req, res) {
+    try {
+        const result = await User.create(req.body);
+        res.send(result);
+    }
+    catch (e) {
+        errorHandler(res, 500, e.message || "Some error occurred while creating the User.")
+    }
+}
+
+async function findOne (req, res) {
+    const { id } = req.params;
+
+    try {
+        const result = await User.findByPk(id);
+        res.send(result);
+    }
+    catch (e) {
+        errorHandler(res, 500, `Error retrieving User with id=${id}`)
+    }
+}
+
+async function findAll (req, res) {
+    try {
+        const result = await User.findAll({where: req.query});
+        res.send(result);
+    }
+    catch (e) {
+        errorHandler(res, 500,  e.message || "Some error occurred while retrieving tutorials.")
+    }
+}
+
+async function update (req, res) {
+    const id = req.params.id;
+
+    try {
+        let result = await User.update(req.body, {where: {id}, returning: true, plain: true});
+        let [ count, user ] = result;
+        user = user?.dataValues || null;
+        res.send(user)
+    }
+    catch (e) {
+        errorHandler(res, 500, `Error updating User with id=${id}`)
+    }
+}
+
+async function remove (req, res) {
+    const id = req.params.id;
+
+    try {
+        let result = await User.destroy({where: {id}});
+        let message = result === 1 ?
+            "User was deleted successfully!"
+            :
+            `Cannot delete User with id=${id}. Maybe User was not found!`;
+
+        res.send({message})
+    }
+    catch (e) {
+        errorHandler(res, 500, `Error updating User with id=${id}`)
+    }
 }
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-import { create, findOne, findAll, update, remove, removeAll } from '../controllers/user.controller.js';
+import { create, findOne, findAll, update, remove } from '../controllers/user.controller.js';
 
 router.post("/", create);
 router.get("/", findAll);
 router.get("/:id", findOne);
 router.put("/:id", update);
 router.delete("/:id", remove);
-router.delete("/", removeAll);
 
 export default router;
