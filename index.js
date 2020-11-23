cab287db7c11531d146f18351d887a9027aca6a0
Bloodsfire <voobsheeee@mail.ru>
2020-11-23 15:10:27 +0600

    Config main index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.options('*', cors());
+
+sequelize.sync({force: true}).then(() => {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
 });
